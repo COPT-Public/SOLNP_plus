c625bd63f4d41ebbf79919c86be3890f8f891422
JinsongLiu6 <liujinsong@163.sufe.edu.cn>
2023-02-19 19:26:03 +0800

    modify compile file

--- make_solnp.m
+++ make_solnp.m
@@ -1,27 +1,29 @@
 function [] = make_solnp()
 
+
+platform = convertCharsToStrings(computer('arch'));
+
 mexfname = 'SOLNP_plus';
 
 % change your mkl path here
-% mkl_path = "F:\MKL\mkl\2021.3.0";
-mkl_path = '/nfsshare/software/deng/install/intel/mkl';
+mkl_path = 'path to your mkl root';
 
 % change your osqp path here
-% osqp_path = "F:\GitHub\SOLNP\osqp";
-osqp_path = '/nfsshare/home/liujinsong/github/osqp';
+osqp_path = 'path to your osqp root';
 
-% mkl_lib_path = mkl_path + "\lib\intel64";
-% osqp_lisb_path = osqp_path + "\lib";
 
-mkl_lib_path = fullfile(mkl_path, 'lib', 'intel64');
-osqp_lib_path = fullfile(osqp_path, 'lib');
+if platform == "maci64"
+    mkl_lib_path = fullfile(mkl_path, 'lib');
+else
+    mkl_lib_path = fullfile(mkl_path, 'lib', 'intel64');
+end
+osqp_lib_path = fullfile(osqp_path, 'build', 'out');
 
 
 
 if exist(mkl_path) && exist(osqp_path)
     lib_path = [convertCharsToStrings(mkl_lib_path), convertCharsToStrings(osqp_lib_path)];
     lib_path = join('-L' + lib_path);
-    platform = convertCharsToStrings(computer('arch'));
 else
     error('Please add your pathes to mkl and osqp\n');
 end 
@@ -35,12 +37,9 @@ if platform == "win64"
         ' -lmkl_core', ' -lmkl_sequential '];
 elseif platform == "maci64"
     fprintf('Linking MKL in MacOS \n');
-    link = [link, ' -lmkl_intel_lp64',...
-        ' -lmkl_core', ' -lmkl_sequential '];
+    link = [link, join(mkl_lib_path + ["/libmkl_intel_lp64.a","/libmkl_core.a", "/libmkl_sequential.a "])];
 elseif platform == "glnxa64"
     fprintf('Linking MKL in Linux \n');
-    link = [link, ' -lmkl_intel_lp64',...
-        ' -lmkl_core', ' -lmkl_sequential '];
 
     sys_cmd = ['gcc -fPIC -I', matlabroot, '/extern/include ' , '-I./include -I', mkl_path, '/include -I', osqp_path, '/include ',...
                 '-shared -o ', mexfname, '.mexa64 ', './source/*.c ', '-Wl,--start-group ', mkl_lib_path, '/libmkl_core.a ',...
@@ -55,9 +54,10 @@ end
 
 link = [link, '-losqp'];
 
-psrc = '.\source';  
+psrc = fullfile('.', 'source');  
 
-src_files = dir( [psrc '\*.c'] );
+
+src_files = dir( [psrc '/*.c'] );
 srclist = [];
 
 for i = 1:length(src_files)
@@ -69,13 +69,13 @@ src = fullfile(psrc, srclist);
 pinc = "include";
 
 
-mkl_include = mkl_path + "\include";
-osqp_include = osqp_path + "\include";
+mkl_include = fullfile(mkl_path, "include");
+osqp_include = fullfile(osqp_path, "include");
 
 inc = [pinc,mkl_include,osqp_include];
 inc = join('-I' + inc);
 
-cmd = 'mex -g -output ' + join([mexfname, lib_path, src, inc, link]);
+cmd = 'mex -output ' + join([mexfname, lib_path, src, inc, link]);
 
 fprintf('%s\n',cmd);
 eval(replace(cmd, 'Program Files (x86)', "'Program Files (x86)'"));
